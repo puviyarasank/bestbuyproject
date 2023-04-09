de8e10dcdd54ae37b731bb7c68215e4682cf0204
puviyarasank <106440889+puviyarasank@users.noreply.github.com>
2023-04-09 22:54:36 +0530

    Update Extendreports.java

--- src/main/java/utilities/Extendreports.java
+++ src/main/java/utilities/Extendreports.java
@@ -6,7 +6,7 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 public class Extendreports {
 	public static ExtentReports extendreport() {
 
-		String reportpath = System.getProperty("user.dir") + "\\reports\\extendreport.html";
+		File reportpath = new File("C:\\Users\\PUVI\\eclipse-workspace\\BestBuy\\reports\\extendreports.html");
 		ExtentSparkReporter extend = new ExtentSparkReporter(reportpath);
 		extend.config().setReportName("bestbuy automation report");
 		extend.config().setDocumentTitle("BESTBUY AUTOMATION TESTING");
